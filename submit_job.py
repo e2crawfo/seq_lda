1fa0c81c5a476cfd89ea05fe89bf3fe9cf912e77
Eric Crawford <eric.crawford@mail.mcgill.ca>
2017-03-11 20:04:30 -0500

    Improvements to submit_job.py

--- submit_job.py
+++ submit_job.py
@@ -27,8 +27,11 @@ def make_sym_link(target, name):
     os.symlink(target, name)
 
 
-def main(task, n_jobs, input_zip, n_nodes=1, ppn=12, walltime="1:00:00", add_date=False, test=0, scratch=None, verbose=0):
+def main(
+        task, n_jobs, input_zip, n_nodes=1, ppn=12, walltime="1:00:00",
+        add_date=False, test=0, scratch=None, exclude="", verbose=0, show_script=0):
     name = os.path.splitext(os.path.basename(input_zip))[0]
+    exclude = "--exclude " + exclude if exclude else ""
     kwargs = locals()
     kwargs['n_procs'] = n_nodes * ppn
     kwargs['input_zip'] = os.path.abspath(input_zip)
@@ -36,7 +39,7 @@ def main(task, n_jobs, input_zip, n_nodes=1, ppn=12, walltime="1:00:00", add_dat
     kwargs['original_dir'] = os.path.abspath(os.path.dirname(input_zip))
 
     # Create directory to run the job from - should be on local_scratch.
-    scratch = os.path.abspath(scratch) or os.getenv('SCRATCH')
+    scratch = os.path.abspath(scratch or os.getenv('SCRATCH'))
     experiments_dir = os.path.join(scratch, "experiments")
     scratch = make_directory_name(experiments_dir, name, add_date=add_date)
     kwargs['scratch'] = scratch
@@ -55,6 +58,7 @@ mkdir {scratch}/{local_scratch}'''
 #
 #PBS -l nodes={n_nodes}:ppn={ppn},walltime={walltime}
 #PBS -N {name}
+#PBS -m abe
 #PBS -e stderr.txt
 #PBS -o stdout.txt
 
@@ -65,11 +69,11 @@ module load python/2.7.2'''
 
     if test:
         command = '''
-seq 0 $(({n_jobs}-1)) | parallel --eta --no-notice -j{n_procs} --workdir $PWD sd-experiment {task} {{}} --d {local_scratch}/{name}_{task} --verbose {verbose}
+seq 0 $(({n_jobs}-1)) | time parallel --no-notice -j{n_procs} --workdir $PWD sd-experiment {task} {{}} --d {local_scratch}/{name}_{task} --verbose {verbose} > {scratch}/stdout.txt 2> {scratch}/stderr.txt
 '''
     else:
         command = '''
-seq 0 $(({n_jobs}-1)) | parallel --eta --no-notice -j{n_procs} --sshloginfile $PBS_NODEFILE --workdir $PWD sd-experiment {task} {{}} --d {local_scratch}/{name_{task} --verbose {verbose}
+seq 0 $(({n_jobs}-1)) | time parallel --no-notice -j{n_procs} --sshloginfile $PBS_NODEFILE --workdir $PWD sd-experiment {task} {{}} --d {local_scratch}/{name}_{task} --verbose {verbose}
 ''' # noqa
 
     code = (preamble + '''
@@ -79,27 +83,29 @@ export OMP_NUM_THREADS=1
 
 cd {scratch}
 mpiexec -np {n_nodes} -pernode sh -c 'cp {input_zip} {local_scratch} && \\
-                                      unzip {local_scratch}/{input_zip_bn} -d {local_scratch} && \\
+                                      unzip -q {local_scratch}/{input_zip_bn} -d {local_scratch} && \\
                                       mv {local_scratch}/{name} {local_scratch}/{name}_{task}'
 
 # START PARALLEL JOBS USING NODE LIST IN $PBS_NODEFILE
 ''' + command + '''
 cd {local_scratch}
-mpiexec -np {n_nodes} -pernode sh -c 'zip -r $OMPI_COMM_WORLD_RANK {name}_{task} && \\
+mpiexec -np {n_nodes} -pernode sh -c 'zip -rq $OMPI_COMM_WORLD_RANK {name}_{task} {exclude} && \\
                                       cp $OMPI_COMM_WORLD_RANK.zip {scratch}'
 cd {scratch}
 
 for i in `seq 0 $(({n_nodes}-1))`;
 do
-    unzip $i.zip
+    unzip -qn $i.zip
     rm $i.zip
 done
 
-zip -r {name}_{task} {name}_{task}
+zip -rq {name}_{task} {name}_{task}
 rm -rf {name}_{task}
-cp {name}_{task}.zip {original_dir}
+cp {name}_{task}.zip {original_dir}''')
 
-''').format(**kwargs)
+    code = code.format(**kwargs)
+    if show_script:
+        print(code)
 
     if not os.path.isdir(scratch):
         os.makedirs(scratch)
